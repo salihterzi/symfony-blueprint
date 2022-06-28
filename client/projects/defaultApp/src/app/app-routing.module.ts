48277b3da3fe200b2579ca9ab26bf31bc010c396
salihterzi <salytem@gmail.com>
2022-06-29 00:47:09 +0300

    created login and welcome components

--- client/projects/defaultApp/src/app/app-routing.module.ts
+++ client/projects/defaultApp/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {WelcomeComponent} from "./welcome/welcome.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: WelcomeComponent,
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
